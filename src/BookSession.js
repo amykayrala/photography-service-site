e3be2c1b91b6fe9db3b40c9d39f474716e77001b
Amy Kayrala <amykayrala@gmail.com>
2025-06-06 16:08:13 -0400

    date selected

--- src/BookSession.js
+++ src/BookSession.js
@@ -2,16 +2,18 @@ import React, { useState } from 'react';
 import Calendar from 'react-calendar';
 import 'react-calendar/dist/Calendar.css';
 
-export default function BookSession() {
-    const [value, onChange] = useState(new Date());
+function BookSession() {
+    const [value, onChange] = useState(new Date());
 
-    return (
-        <div>
-            <h1>Calendar - GeeksforGeeks</h1>
-            <Calendar
-                onChange={onChange}
-                value={value}
-            />
-        </div>
-    );
+    return (
+        <div>
+            <Calendar
+                onChange={onChange}
+                value={value}
+            />
+            <p>Selected date: {value.toDateString()}</p>
+        </div>
+    );
 }
+
+export default BookSession;
