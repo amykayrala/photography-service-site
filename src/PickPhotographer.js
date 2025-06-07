5b528934367f5f12e2b7aeaaa851a11f9de95234
Amy Kayrala <amykayrala@gmail.com>
2025-06-07 13:53:54 -0400

    new page for photographer

---
+++ src/PickPhotographer.js
@@ -0,0 +1,11 @@
+import ProgressBar from 'react-bootstrap/ProgressBar';
+
+function PickPhotographer() {
+    <div className='contact-page'>
+        <ProgressBar animated now={33} variant="success" label={' 1. Date and Time'} />
+
+    </div>
+    
+}
+
+export default PickPhotographer;
